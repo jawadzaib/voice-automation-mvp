144206ea5c7d203fef40e3b82edb15bedc6fd413
Jawad Zaib <jawadzaib3@gmail.com>
2025-07-16 21:24:46 +0500

    mvp backup data

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,21 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { HealthController } from './controllers/health.controller';
 import { MvpModule } from './mvp/mvp.module';
 
 @Module({
-  imports: [MvpModule],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'postgres',
+      port: 5432,
+      username: 'root',
+      password: 'root',
+      synchronize: true,
+      entities: [__dirname + '/**/*.entity{.ts,.js}'],
+    }),
+    MvpModule
+  ],
   controllers: [HealthController],
   providers: [],
 })
--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,30 @@
 import { NestFactory } from '@nestjs/core';
 import * as cors from 'cors';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 import { AppExceptionFilter } from './filters/app-exception.filter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const port = process.env.PORT ?? 3000;
+
+  const config = new DocumentBuilder()
+    .setTitle('Voice Automation MVP')
+    .setDescription('API documentation')
+    .setVersion('1.0')
+    .addTag('MVP')
+    // .addBearerAuth(
+    //   {
+    //     type: 'http',
+    //     scheme: 'bearer',
+    //     bearerFormat: 'JWT',
+    //   },
+    //   'authorization',
+    // )
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
 
   app.use(cors());
 
